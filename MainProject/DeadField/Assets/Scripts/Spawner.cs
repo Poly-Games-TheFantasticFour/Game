7787ca9ddb3aa634f5c4c64a44c7c581adeecef1
unknown <cedrickbusk@gmail.com>
2015-03-11 00:24:41 -0400

    Debut powerUp (spawn)

---
+++ MainProject/DeadField/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject powerUp;
+	public float premierSpawn = 3f;
+	public float intervalleEntreSpawn = 3f;
+
+
+	void Start () {
+			InvokeRepeating("spawn", premierSpawn /*temps avant 1er spawn*/ , intervalleEntreSpawn /*intervalle entre les spawns*/);
+	}
+	
+
+	void spawn () 
+	{
+		 {
+			Vector3 spawnPoint = new Vector3 (Random.Range (-10.0F, 10.0F), 2.5F, Random.Range (-10.0F, 10.0F)); 
+			Instantiate (powerUp, spawnPoint/*endroit*/, transform.rotation);
+		} 
+
+	}
+}
