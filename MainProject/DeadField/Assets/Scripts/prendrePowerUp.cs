f04254504e55698b29c94d983d57c5c1bbe9012b
unknown <cedrickbusk@gmail.com>
2015-03-12 08:35:05 -0400

    update powerUp Scale

--- MainProject/DeadField/Assets/Scripts/prendrePowerUp.cs
+++ MainProject/DeadField/Assets/Scripts/prendrePowerUp.cs
@@ -1,16 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class prendrePowerUp : MonoBehaviour {
+
+	float tempsActivation = 0.0f; 
+	bool estActif = false;
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "powerUp") {
 			other.gameObject.SetActive(false);
+			tempsActivation +=10.0f;
+			if(estActif == true){
+				tempsActivation += 10.0f;
+			}
+			else {
+				transform.localScale += new Vector3 (3.0F, 3.0F, 3.0F); 
+			}
+			StartCoroutine (actif());
 		}
-
 	}
 
+	IEnumerator actif(){
+	
+		estActif = true;
+		float time = 0;
+		while (time < (tempsActivation * Time.deltaTime)) {
+			time += Time.deltaTime;
+			yield return new WaitForSeconds(1.0F);
+		}
+		if (time >= (tempsActivation * Time.deltaTime)) {
+			estActif = false;
+			tempsActivation = 0.0f;
+			transform.localScale -= new Vector3 (3.0F,3.0F,3.0F);
+
+		}
+	}
 
 
 
