432363c51164206959bcffdcab3174be78f57ec2
Julien Charbonenau <julien_charbonneau@live.ca>
2015-03-12 16:58:34 -0400

    Ménage script

--- MainProject/DeadField/Assets/Scripts/FallingPlateform.cs
+++ MainProject/DeadField/Assets/Scripts/FallingPlateform.cs
@@ -7,56 +7,22 @@ public class FallingPlateform : MonoBehaviour {
 	public int numberPoolSize = 25;
 	int keyNumber = 7;
 	int number = 0;
-	//float time = 0f;
-
 
 	void Awake () 
 	{
 		StartCoroutine (actif());
 	}
-
-	/*void fixedUpdate () 
-	{
-		time += Time.deltaTime;
-	}*/
-
+	
 	IEnumerator actif()
 	{
 		while (number != keyNumber) 
 		{
-			//if (time >= timeBetweenNumber)
-			//{
 				number = Random.Range(0, numberPoolSize);
-			//	time = 0f;
-		//	}
+
 			yield return new WaitForSeconds(timeBetweenNumber);
 		}
 		rigidbody.useGravity = true;
 		rigidbody.isKinematic = false;
 	}
-
-
-
-
-
-
-	// Update is called once per frame
-	/*void fixedUpdate () 
-	{
-		timer += Time.deltaTime;
-		if (timer >= timeBetweenNumber && Time.timeScale != 0) 
-			generateNumber ();
-		if (number == keyNumber) 
-		{
-			rigidbody.useGravity = true;
-			rigidbody.isKinematic = false;
-		}
-	
-	}
-
-	void generateNumber()
-	{
-		number = Random.Range(0, numberPoolSize);
-	}*/
 
 }
