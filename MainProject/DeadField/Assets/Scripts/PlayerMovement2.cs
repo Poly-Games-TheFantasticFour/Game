ee21b57545003f552c178237387ecdf3fc9fe49a
Julien Charbonenau <julien_charbonneau@live.ca>
2015-03-13 12:03:18 -0400

    Playable second player (arrows (move/rotate), keypad0 (jump), keypadEnter (attack)). bug fix : player stuck into a wall. bug fix: jump higher if near a wall.

--- MainProject/DeadField/Assets/Scripts/PlayerMovement2.cs
+++ MainProject/DeadField/Assets/Scripts/PlayerMovement2.cs
@@ -1,21 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour
-{
-	//public float turnSmoothing = 15f;
+public class PlayerMovement2 : MonoBehaviour {
+	public float turnSmoothing = 15f;
 	public static float speed = 8.0f; //besoin detre static pour pouvoir sen servir dansun autre script
 	public float jumpForce = 50.0f;
 	public static float attactForce = 50.0f;
 	public static float timeBetweenAttacks = 0.88f;
 	public float attactRange = 2.0f;
 	public float jumpCheckDist = 1.0f;
-
+	
 	//bool isGrounded = true;
 	int floorMask, hitMask, jumpMask;
-	float camRayLength = 200f;
+	//float camRayLength = 200f;
 	float timer = 0.0f;
-
+	
 	Rigidbody playerRigidbody;
 	Animator anim;
 	RaycastHit shootHit, shootJump;
@@ -28,35 +27,35 @@ public class PlayerMovement : MonoBehaviour
 		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
 	}
-
+	
 	void FixedUpdate ()
 	{
 		timer += Time.deltaTime;
 		
 		playerRigidbody.AddForce(Physics.gravity * playerRigidbody.mass);
-		float h = Input.GetAxis ("Horizontal");
-		float v = Input.GetAxis ("Vertical");
-
-		if (Input.GetButton("Jump") && IsGrounded()) 
+		float h = Input.GetAxis ("Horizontal2");
+		float v = Input.GetAxis ("Vertical2");
+		
+		if (Input.GetKey(KeyCode.Keypad0) && IsGrounded()) 
 		{
 			Jump ();
 			//isGrounded = false;
 		}
-
+		
 		Move (h, v);
-		Turning ();
-
-		if (Input.GetButtonDown("Fire1")&& timer >= timeBetweenAttacks && Time.timeScale != 0) 
+		//Turning ();
+		
+		if (Input.GetKey(KeyCode.KeypadEnter) && timer >= timeBetweenAttacks && Time.timeScale != 0) 
 		{
 			timer = 0.0f;
 			anim.SetTrigger ("Attack");
 			Invoke("Attack", .5f);
 		}
-
-		/*if (h != 0 || v != 0)
-			Rotate (h, v);*/
+		
+		if (h != 0 || v != 0)
+			Rotate (h, v);
 	}
-
+	
 	bool IsGrounded()
 	{
 		Vector3 DirectionRay = transform.TransformDirection(Vector3.down);
@@ -64,14 +63,14 @@ public class PlayerMovement : MonoBehaviour
 		Vector3 left = new Vector3(-0.25f,1.0f,0f);
 		Vector3 front = new Vector3(0.0f,1.0f,0.25f);
 		Vector3 rear = new Vector3(0.25f,1.0f,-0.25f);
-
+		
 		Debug.DrawRay(transform.position + right, DirectionRay * jumpCheckDist, Color.blue);
 		Debug.DrawRay(transform.position + left, DirectionRay * jumpCheckDist, Color.blue);
 		Debug.DrawRay(transform.position + front, DirectionRay * jumpCheckDist, Color.blue);
 		Debug.DrawRay(transform.position + rear, DirectionRay * jumpCheckDist, Color.blue);
-
+		
 		if(Physics.Raycast (transform.position + right, DirectionRay, out shootJump, jumpCheckDist, jumpMask))
-		   return true;
+			return true;
 		if(Physics.Raycast (transform.position + left, DirectionRay, out shootJump, jumpCheckDist, jumpMask))
 			return true;
 		if(Physics.Raycast (transform.position + front, DirectionRay, out shootJump, jumpCheckDist, jumpMask))
@@ -80,22 +79,22 @@ public class PlayerMovement : MonoBehaviour
 			return true;
 		return false;
 	}
-
+	
 	void Jump () 
 	{
 		playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 	}
-
+	
 	void Move (float h, float v)
 	{
 		Vector3 move = new Vector3 (h, 0.0f, v);
 		transform.position += Vector3.ClampMagnitude (move, 1.0f) * speed * Time.deltaTime;
-
+		
 		bool running = h != 0f || v != 0f;
 		anim.SetBool ("IsRunning", running);
 	}
-
-	void Turning()
+	
+	/*void Turning()
 	{
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit floorHit;
@@ -107,27 +106,27 @@ public class PlayerMovement : MonoBehaviour
 			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
 			playerRigidbody.MoveRotation(newRotation);
 		}
-	}
-
+	}*/
+	
 	void Attack()
 	{
 		Vector3 DirectionRay = transform.TransformDirection(Vector3.forward);
 		Debug.DrawRay(transform.position + transform.up, DirectionRay * attactRange , Color.blue);
-
+		
 		if(Physics.Raycast (transform.position + transform.up, DirectionRay, out shootHit, attactRange, hitMask))
 		{
 			shootHit.rigidbody.AddForce(DirectionRay.normalized * attactForce, ForceMode.Impulse); 
 		}
-
+		
 	}
-				
+	
 	/*void OnCollisionEnter()
 	{
 		isGrounded = true;
 	}*/
-
+	
 	// ************************Old rotate (via wasd)*************************
-	/*void Rotate (float horizontal, float vertical)
+	void Rotate (float horizontal, float vertical)
 	{
 		Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
 
@@ -135,5 +134,5 @@ public class PlayerMovement : MonoBehaviour
 		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
 
 		rigidbody.MoveRotation(newRotation);
-	}*/
+	}
 }
