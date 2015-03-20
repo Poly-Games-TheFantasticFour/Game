fd978207ef630454e5415c747892774352a464b0
Julien Charbonneau <julien_charbonneau@live.ca>
2015-03-20 12:26:25 -0400

    Restart game if all players are dead + HUD (in progress)

---
+++ MainProject/DeadField/Assets/Scripts/EndGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	public float restartTimer = 3.0f;
+	GameObject player1, player2;
+
+	// Use this for initialization
+	void Start () {
+		player1 = GameObject.Find("Zombie");
+		player2 = GameObject.Find ("Monster");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (player1 == null && player2 == null)
+			Invoke ("Restart", restartTimer);
+	}
+
+	void Restart(){
+		Application.LoadLevel("Arena_Cage");
+	}
+}
