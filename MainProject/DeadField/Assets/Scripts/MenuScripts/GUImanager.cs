4b47baf0a60a5697ef87af4d20de48a3ed39fa3e
kimsonvo <david_vo@hotmail.ca>
2015-03-28 23:11:45 -0400

    menu...ver.2.257

---
+++ MainProject/DeadField/Assets/Scripts/MenuScripts/GUImanager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUImanager : MonoBehaviour {
+	
+	public GameObject loadingImage;
+	public Animator startButton;
+	public Animator settingsButton;
+	public Animator deathCageButton;
+	public Animator exitButton;
+	public Animator returnButton;
+
+
+	public void OpenSettings()
+
+	{
+		startButton.SetBool("isHidden", true);
+		settingsButton.SetBool("isHidden",true);
+		exitButton.SetBool("isHidden",true);
+		returnButton.SetBool ("isHidden", true);
+		audio.Play ();
+
+
+	}
+
+	public void openGameSelection()
+
+	{
+		startButton.SetBool("isHidden", true);
+		settingsButton.SetBool("isHidden",true);
+		exitButton.SetBool("isHidden",true);
+		deathCageButton.SetBool ("isHidden", true);
+		returnButton.SetBool ("isHidden", true);
+		audio.Play ();
+
+
+	}
+
+	public void returnSelection() { 
+	
+	
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden",false);
+		exitButton.SetBool("isHidden",false);
+		deathCageButton.SetBool ("isHidden", false);
+		returnButton.SetBool ("isHidden", false);
+	
+	}
+
+	public void LoadScene(int level)
+	{
+		loadingImage.SetActive(true);
+		Application.LoadLevel(level);
+	}
+
+	public void ApplicationQuit (){
+
+		Application.Quit ();
+
+	}
+}
+
+
