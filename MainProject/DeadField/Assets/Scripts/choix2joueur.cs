cd1155d301aaa60f0c846df5ec3067d00c4f4341
Julien Charbonneau <julien_charbonneau@live.ca>
2015-04-07 22:59:45 -0400

    Well it is time for a new build, a special one this time: Beta v0.1.0! :D

---
+++ MainProject/DeadField/Assets/Scripts/choix2joueur.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class choix2joueur : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GUImanager.njoueur == 2) {
+			Destroy (gameObject);
+		}
+	}
+	
+}
