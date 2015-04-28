1ddee1d98b950b20144742e93ce12ef5528272bf
Julien Charbonneau <julien_charbonneau@live.ca>
2015-04-28 12:20:49 -0400

    4 players fix, build + web release v1.0.1

--- MainProject/DeadField/Assets/Scripts/Player4/PlayerMovement4.cs
+++ MainProject/DeadField/Assets/Scripts/Player4/PlayerMovement4.cs
@@ -25,7 +25,7 @@ public class PlayerMovement4 : MonoBehaviour
 
 	char NbController ()
 	{
-		if (Input.GetJoystickNames ().Length < 1)
+		if (Input.GetJoystickNames ().Length < 4)
 			return 'K';
 		else
 			return 'C';
